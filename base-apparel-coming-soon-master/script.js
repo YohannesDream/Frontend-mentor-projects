b75b1f67a0f9aa9f044ab3c2b5c159209e24dc19
Yohannes Merete <152685312+YohannesDream@users.noreply.github.com>
2024-03-23 10:40:24 +0300

    Add files via upload

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,21 @@
+ var inputEmail = document.getElementById("input-email");
+ var inputHolder = document.getElementById("input-holder");
+ var submitEmail = document.getElementById("submit-email");
+ var errorEmail = document.getElementById("error-email");
+
+ submitEmail.addEventListener("click", function(){
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailPattern.test(inputEmail.value)){
+        inputEmail.classList.add("error-input");
+        errorEmail.style.display = "block";
+        inputHolder.style.borderWidth = "2px";
+        inputHolder.style.borderColor = "red";
+
+        /* errorMessage(email, 'Look like this is not an email');
+        email.classList.add('error-input'); */
+    }
+    else {
+        document.write(inputEmail.value)
+    }
+ })
+  
